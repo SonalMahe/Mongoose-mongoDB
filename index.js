9e7ac1d3c36de9bbf6420d67bd45588578c0cc9e
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-04-23 13:50:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ app.delete('/items/:id', async (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 }); 
